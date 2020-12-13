2873e9a9d787de7cfa139ef8c3a28e009bf7da94
Konstantin Biriukov <kostya-leon@mail.ru>
2020-12-13 14:34:41 -0600

    comments changes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var months = [
 ];
 
 function filterBy(month) {
-    var filters = ['==', 'month', month];
+    let filters = ['==', 'month', month];
     map.setFilter('earthquake-circles', filters);
     map.setFilter('earthquake-labels', filters);
 
@@ -31,22 +31,21 @@ function filterBy(month) {
 }
 
 map.on('load', function () {
-    // Data courtesy of http://earthquake.usgs.gov/
-    // Query for significant earthquakes in 2015 URL request looked like this:
+    // Data: http://earthquake.usgs.gov/
+    // Query for significant earthquakes in 2020 : (geojson with start- and end-time, and minmagnitude)
     // http://earthquake.usgs.gov/fdsnws/event/1/query
     //    ?format=geojson
-    //    &starttime=2015-01-01
-    //    &endtime=2015-12-31
+    //    &starttime=2020-01-01
+    //    &endtime=2020-12-31
     //    &minmagnitude=6'
-    //
-    // Here we're using d3 to help us make the ajax request but you can use
-    // Any request method (library or otherwise) you wish.
+
+    // d3.js library is used to help making the ajax request
     d3.json(
         'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-12-31&minmagnitude=6',
         function (err, data) {
             if (err) throw err;
 
-            // Create a month property value based on time used to filter against.
+            // Create a month property value based on time used for filter.
             data.features = data.features.map(function (d) {
                 d.properties.month = new Date(d.properties.time).getMonth();
                 return d;
@@ -98,27 +97,23 @@ map.on('load', function () {
                         'Open Sans Bold',
                         'Arial Unicode MS Bold'
                     ],
-                    'text-size': 12
+                    'text-size': 13
                 },
                 'paint': {
                     'text-color': 'rgba(0,0,0,0.5)'
                 }
             });
 
-            // Set filter to first month of the year
-            // 0 = January
+            // Set filter to the first month of the year; 0 = January
             filterBy(0);
 
             map.on('click', 'earthquake-circles', function (e) {
                 let coordinates = e.features[0].geometry.coordinates.slice();
-                // var description = e.features[0].properties.title;
-                var link = e.features[0].properties.url;
-                var description = '<a href="' + link + '" target="blank">' + e.features[0].properties.title + '</a>';
-
+                let link = e.features[0].properties.url;
+                let description = '<a href="' + link + '" target="blank">' + e.features[0].properties.title + '</a>';
 
                 // Ensure that if the map is zoomed out such that multiple
-                // copies of the feature are visible, the popup appears
-                // over the copy being pointed to.
+                // copies of the feature are visible, the popup appears over the copy being pointed to.
                 while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
                     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                 }
